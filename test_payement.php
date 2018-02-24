814c3cf17bef0e93eb117460413d3196b06e9229
Alexandre Louf <alexandre.louf@eemi.com>
2018-02-24 16:36:28 +0100

    al

---
+++ test_payement.php
@@ -0,0 +1,13 @@
+<form action="https://www.coinpayments.net/index.php" method="post">
+    <input type="hidden" name="cmd" value="_pay">
+    <input type="hidden" name="reset" value="1">
+    <input type="hidden" name="merchant" value="9c63f50e1711884d9609b95ef551a7c5">
+    <input type="hidden" name="item_name" value="Abonnement Premium">
+    <input type="hidden" name="currency" value="EUR">
+    <input type="hidden" name="amountf" value="15.00000000">
+    <input type="hidden" name="quantity" value="1">
+    <input type="hidden" name="allow_quantity" value="0">
+    <input type="hidden" name="want_shipping" value="1">
+    <input type="hidden" name="allow_extra" value="0">
+    <input type="image" src="https://www.coinpayments.net/images/pub/buynow-wide-blue.png" alt="Acheter Maintenant avec CoinPayments.net">
+</form>
