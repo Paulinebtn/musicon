16e31b5fc7dba2d0246c3e4f680292aaa40030a4
Jacques Hovine <jacques.hovine@eemi.com>
2018-02-24 15:20:15 +0100

    Connexion à la base 1
    Ajout des fichiers de connexion à la base.
    Création de la page d'accueil du back office (admin.php) en statique.

---
+++ admin/admin.php
@@ -0,0 +1,84 @@
+<?php
+require_once('../include/init.inc.php'); //Connexion à la base
+
+/*
+if($_SESSION['login'])
+{
+  header('Location:admin/home_back.php');
+}
+
+if(isset($_GET))
+{
+  if(!empty($_GET['action']) && $_GET['action'] === 'logout')
+  {
+    $confirmation = '<h3 class="center green-text">Déconnecté avec succès</h3>';
+  }
+  else if(!empty($_GET['action']) && $_GET['action'] === 'redirect')
+  {
+    $confirmation = '<h3 class="center red-text">Connexion requise !</h3>';
+  }
+}
+
+if($_POST)
+{
+  if(!empty($_POST['login']) && !empty($_POST['password']))
+  {
+    $check = $pdo->prepare('SELECT * FROM info_login WHERE login = :login');
+    $check->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+    $check->execute();
+
+    $check_user = $check->fetch(PDO::FETCH_ASSOC);
+
+    if($check_user['password'] === $_POST['password'])
+    {
+      $_SESSION['login'] = $check_user['login'];
+      header('Location:admin/home_back.php');
+    }
+    else
+    {
+      $confirmation = '<h3 class="center red-text">Login ou mot de passe incorrect</h3>';
+    }
+  }
+  else
+  {
+    $confirmation = '<h3 class="center red-text">Veuillez remplir tous les champs !</h3>';
+  }
+}
+*/
+
+echo $_SERVER['DOCUMENT_ROOT'];
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+
+  <main>
+    <div>
+      <h1>Bienvenue dans le Back Office</h1>
+    </div>
+
+    <?php// if(!empty($confirmation)){echo $confirmation;} ?>
+
+    <section>
+        <form method="post" action="#">
+          <div>
+            <input id="login" name="login" type="text" required>
+            <label for="login">Login</label>
+          </div>
+          <div class="input-field col s12">
+            <input id="password" name="password" type="password" required>
+            <label for="password">Password</label>
+          </div>
+          <div class="center">
+            <button type="submit" name="connexion">Connexion</button>
+          </div>
+        </form>
+    </section>
+  </main>
+</body>
+</html>
---
+++ include/config.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+define("DB_NAME","muzicon");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
