99f4183a70a4b98360dccbf40fcbe7e9c15f2670
Jacques Hovine <jacques.hovine@eemi.com>
2018-02-24 16:02:54 +0100

    Connexion admin
    Création et modification des fichiers nécessaires à la connexion administrateur. Affichage d'un message rentré en dur dans la BDD.

--- include/init.inc.php
+++ include/init.inc.php
@@ -3,6 +3,6 @@
 $pdo = new Pdo('mysql:host=localhost;dbname=' . DB_NAME, DB_USER, DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 //var_dump($pdo);
 
-//const PUBLIC_URL =;
+//const PUBLIC_URL = $_SERVER['DOCUMENT_ROOT'];
 
 session_start();
