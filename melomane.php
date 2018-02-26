920bdb731c17ebbdcd1c81bee35b04e967ba567f
Paulinebtn <pauline.bittante@eemi.com>
2018-02-26 15:29:48 +0100

    po

--- melomane.php
+++ melomane.php
@@ -2845,6 +2845,12 @@
                 </div>
             </div>
         </section>
+        <!-- Section 3 : mon compte -->
+        <section class="account">
+	        <div class="ligne-artiste">
+                <h2>Mon compte</h2>
+            </div>
+        </section>
     </main>
 
     <div class="player2">
