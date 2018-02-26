4278d06df6a7cb08d2dd99260e9e2862d271afa4
Paulinebtn <pauline.bittante@eemi.com>
2018-02-26 20:27:24 +0100

    connexion

---
+++ inscription.php
@@ -0,0 +1,22 @@
+!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Muzic On, personnalisez votre musique</title>
+	<meta name="description" content="Muzic On page d'accueil">
+	
+	<!--icones-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/themify-icons.css" rel="stylesheet">
+	<link rel="icon" type="image/png" href="img/logo-musicon.png">
+	
+	<!--forme & responsive-->
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body class="melomane">
+	<main>
+	</main>
+</body>
+</html>
