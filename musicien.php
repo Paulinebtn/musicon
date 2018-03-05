07de0adc45ed5412a46fc1cee0f660b535ec0ac3
Paulinebtn <pauline.bittante@eemi.com>
2018-03-05 13:11:31 +0100

    modif

--- musicien.php
+++ musicien.php
@@ -49,25 +49,7 @@
     </aside>
     
     <main>
-        <div class="head-melomane">
-            <div>
-                <div>
-                    <input type="text" placeholder="Rechercher un artiste, un genre, une musique, un instrument…">
-                    <i class="ti-search"></i>
-                </div>
-                <a href="melomane.php"><button class="music-space button">Écouter de la musique</button></a>
-            </div>
-            <div>
-                <button>Sortie</button>
-                <button>Genre</button>
-                <button>Top</button>
-                <button>Mood</button>
-                <button>Instru</button>
-            </div>
-        </div>
-        <section>
-            <div>salut</div>
-        </section>
+        <!-- les sections sont appelées en AJAX -->
     </main>
     
     <div class="player2">
