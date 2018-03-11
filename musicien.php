f91d7ae97ced03be6335ac5f2b758507e1907589
Paulinebtn <pauline.bittante@eemi.com>
2018-03-11 20:25:25 +0100

    filtres

--- musicien.php
+++ musicien.php
@@ -250,6 +250,206 @@ echo "<input type='hidden' value='". $_SESSION["login_user"] ."' id='pls'>";
                 </div>
             </div>
         </section>
+        <div class="filtre">
+            <div class="filtre-sortie">
+                <ul>
+                    <li>Sortie du jour</li>
+                    <li>Sortie de la semaine</li>
+                    <li>Sortie du mois</li>
+                    <li>Sortie de l'année 2017</li>
+                </ul>
+            </div>
+            <div class="filtre-genre">
+               <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Pop</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Rock</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Hip-hop</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Rap</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Variété</label>
+                    </div>
+               </div>
+               <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Techno</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>RnB</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Jazz</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Latino</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Electro</label>
+                    </div>
+               </div>
+               <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Classique</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Soul</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Reggae</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Country</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Accoustique</label>
+                    </div>
+               </div>
+            </div>
+            <div class="filtre-top">
+                <ul>
+                    <li>Les plus aimées</li>
+                    <li>Les plus écoutées</li>
+                    <li>Les meilleures collaborations</li>
+                    <li>Top artistes muzic on</li>
+                </ul>
+            </div>
+            <div class="filtre-mood">
+                <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Douceur</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Chill</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Fête</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Motivation</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Romantique</label>
+                    </div>
+               </div>
+               <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Mélancolie</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Concentration</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Sunny</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Aphrodite</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Dreamy</label>
+                    </div>
+               </div>
+            </div>
+            <div class="filtre-instru">
+                <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Piano</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Batterie</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Guitare accoustique</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Saxophone</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Basse</label>
+                    </div>
+               </div>
+               <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Trompette</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Violon</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Clavier midi</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Flûte</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Ukulélé</label>
+                    </div>
+               </div>
+               <div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Guitare éléctrique</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Congas</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Accordéon</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Kazoo</label>
+                    </div>
+                    <div>
+                        <input type="checkbox">
+                        <label>Acapela</label>
+                    </div>
+               </div>
+            </div>
+        </div>
     </main>
     
     <div class="player2">
